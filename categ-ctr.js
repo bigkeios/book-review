b8e75cd511bc2f6793149d07eb1cf9ba666d64d8
bigkeios <cqphuong97@gmail.com>
2018-03-30 11:02:44 +0700

    Finish the API to load catetogies by post id they belong to

--- categ-ctr.js
+++ categ-ctr.js
@@ -26,5 +26,31 @@ module.exports =
         {
             res.send(msgFail);   
         });
+    },
+    listCategsByPostID: function(req, res)
+    {
+        let promise = new Promise(function(resolve, reject)
+        {
+            connection.query('SELECT category.name name FROM posts_has_category natural join category WHERE posts_has_category.idposts=?;', [req.params.post_id], function(err, rows, fields)
+            {
+                console.log(this.sql);
+                if(err)
+                {
+                    console.log('Error querying');
+                    return reject(new Error('Error query'));
+                }
+                else
+                {
+                    return resolve(rows);
+                }
+            });
+        });
+        promise.then(function(msgSuccess)
+        {
+            res.send(msgSuccess);
+        }, function(msgFail)
+        {
+            res.send(msgFail);
+        })
     }
 }
--- categ-router.js
+++ categ-router.js
@@ -2,4 +2,5 @@ module.exports = function(app)
 {
     var categCtr = require('./categ-ctr');
     app.route('/api/categs').get(categCtr.listAllCategs);
+    app.route('/api/categs/:post_id').get(categCtr.listCategsByPostID);
 }
