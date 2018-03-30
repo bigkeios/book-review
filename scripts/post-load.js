9c7211add163252de86d3c3a00cb1affeaf6e055
bigkeios <cqphuong97@gmail.com>
2018-03-30 11:03:36 +0700

    finish script to load categories into the view of a post

--- scripts/post-load.js
+++ scripts/post-load.js
@@ -4,28 +4,46 @@ window.onload = function()
     var url = window.location.href;
     var urlSplitted = url.split('/')
     var postID = urlSplitted[4];
-    var request = new XMLHttpRequest();
-    // request.open('GET', '../static data/posts.json', true);
-    request.open('GET', 'http://localhost:8000/api/posts/' + postID);
-    request.onload = function () 
+    let promise = new Promise(function(resolve, reject)
     {
-        var jsonData = JSON.parse(this.response);
-        populatePost(jsonData[0]);
-    }
-    function populatePost(data)
+        var requestPost = new XMLHttpRequest();
+        // request.open('GET', '../static data/posts.json', true);
+        requestPost.open('GET', 'http://localhost:8000/api/posts/' + postID);
+        requestPost.onload = function () 
+        {
+            var jsonData = JSON.parse(this.response);
+            populatePost(jsonData[0]);
+        }
+        function populatePost(data)
+        {
+            post.item(0).setAttribute('id',data.idposts + '');
+            // elements in the post body
+            var postTitle = document.createElement('h3');
+            var detail = document.createElement('h4');
+            var postContent = document.createElement('p');
+            postTitle.textContent = data.title;
+            post.item(0).appendChild(postTitle);
+            var dateCreated = new Date(data.dateCreated);
+            detail.textContent = 'Posted on ' + dateCreated.toDateString();
+            post.item(0).appendChild(detail);
+            postContent.textContent = data.content;
+            post.item(0).appendChild(postContent);
+        }
+        requestPost.send();    
+    });
+    var requestCateg = new XMLHttpRequest();
+    requestCateg.open('GET','http://localhost:8000/api/categs/'+postID);
+    requestCateg.onload = function()
     {
-        post.item(0).setAttribute('id',data.idposts + '');
-        // elements in the post body
-        var postTitle = document.createElement('h3');
-        var detail = document.createElement('h4');
-        var postContent = document.createElement('p');
-        postTitle.textContent = data.title;
-        post.item(0).appendChild(postTitle);
-        var dateCreated = new Date(data.dateCreated);
-        detail.textContent = 'Posted on ' + dateCreated.toDateString();
-        post.item(0).appendChild(detail);
-        postContent.textContent = data.content;
-        post.item(0).appendChild(postContent);
+        var categs = JSON.parse(this.response);
+        var categsDetails = document.createElement('p');
+        categsDetails.textContent = 'Categories: ';
+        for(var categ of categs)
+        {
+            console.log(categ);
+            categsDetails.textContent += categ.name + '';
+        }
+        post.item(0).appendChild(categsDetails);
     }
-    request.send();
+    requestCateg.send();
 }
